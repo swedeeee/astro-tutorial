1c8c88f451cf4c3dbb7a85dacb13e3e2ab58218a
Kent <32994220+swedeeee@users.noreply.github.com>
2026-05-01 14:45:39 +1200

    Fix url position in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
-site: "https://astro-tutorial-swedee.netlify.app";
+export default defineConfig({
+  site: "https://astro-tutorial-swedee.netlify.app",
+});
