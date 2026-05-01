6d765860df65402d767b0683c3024b1651732553
Kent <32994220+swedeeee@users.noreply.github.com>
2026-05-01 19:42:06 +1200

    Updated page slug to blog

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -11,7 +11,7 @@ export async function GET(context) {
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
-      link: `/posts/${post.id}/`,
+      link: `/blog/${post.id}/`,
     })),
     customData: `<language>en-us</language>`,
   });
